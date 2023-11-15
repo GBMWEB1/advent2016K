d8bc339160f8fbe2dfa84a74a7f308b4a8cb86b2
martwebs <mwebster1973@gmail.com>
2023-11-15 22:17:52 +0000

    Refactor

--- src/main/kotlin/Day1.kt
+++ src/main/kotlin/Day1.kt
@@ -33,65 +33,28 @@ class Day1 {
 
     class Grid {
         private var direction = Direction.NORTH
-        private var location = Pair(0, 0)
         private var visited = mutableListOf(Pair(0, 0))
         fun move(command: String){
             this.direction = direction.turn(command[0])
             val distance = command.substring(1).toInt()
-            location = move(distance)
+
+            for (i in 1.. distance) {
+                this.visited.add(move())
+            }
         }
 
-        private fun move(distance: Int): Pair<Int, Int> {
+        private fun move(): Pair<Int, Int> {
+            val location = visited.last()
             return when (this.direction) {
-                Direction.NORTH -> moveNorth(distance)
-                Direction.SOUTH -> moveSouth(distance)
-                Direction.EAST -> moveEast(distance)
-                Direction.WEST -> moveWest(distance)
+                Direction.NORTH -> Pair(location.first, location.second + 1)
+                Direction.SOUTH -> Pair(location.first, location.second - 1)
+                Direction.EAST -> Pair(location.first +1, location.second)
+                Direction.WEST -> Pair(location.first -1, location.second)
             }
-        }
-
-        private fun moveNorth(distance: Int): Pair<Int, Int> {
-            val startY = location.second
-            val endY = location.second + distance
-
-            for (i in startY+1..endY) {
-                visited.add(Pair(location.first, i))
-            }
-            return Pair(location.first, endY)
-        }
-
-        private fun moveSouth(distance: Int): Pair<Int, Int> {
-            val startY = location.second
-            val endY = location.second - distance
-
-            for (i in startY-1 downTo endY) {
-                visited.add(Pair(location.first, i))
-            }
-            return Pair(location.first, endY)
-        }
-
-        private fun moveEast(distance: Int): Pair<Int, Int> {
-            val startX = location.first
-            val endX = location.first + distance
-
-            for (i in startX+1..endX) {
-                visited.add(Pair(i, location.second))
-            }
-            return Pair(endX, location.second)
-        }
-
-        private fun moveWest(distance: Int): Pair<Int, Int> {
-            val startX = location.first
-            val endX = location.first - distance
-
-            for (i in startX-1 downTo endX) {
-                visited.add(Pair(i, location.second))
-            }
-            return Pair(endX, location.second)
         }
 
         fun getBlocksAway(): Int {
-            return getBlocksAway(location)
+            return getBlocksAway(visited.last())
         }
         private fun getBlocksAway(pos: Pair<Int,Int>): Int {
             return abs(pos.first) + abs(pos.second)
