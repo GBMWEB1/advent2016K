6cd9f73bd20507821c0b1730e5b67b69a8b945cb
martwebs <mwebster1973@gmail.com>
2023-11-15 21:57:29 +0000

    Refactor

--- src/main/kotlin/Day1.kt
+++ src/main/kotlin/Day1.kt
@@ -1,9 +1,105 @@
+import kotlin.math.abs
+
 class Day1 {
-    fun day1(): Int {
-        return 10
+    enum class Direction {
+        NORTH, SOUTH, EAST, WEST;
+
+        fun turn(orientation: Char): Direction {
+            return when (orientation) {
+                'R' -> turnRight()
+                'L' -> turnLeft()
+                else ->{ throw Exception("Invalid orientation") }
+            }
+        }
+
+        private fun turnLeft(): Direction {
+            return when (this) {
+                NORTH -> WEST
+                SOUTH -> EAST
+                EAST -> NORTH
+                WEST -> SOUTH
+            }
+        }
+
+        private fun turnRight(): Direction {
+            return when (this) {
+                NORTH -> EAST
+                SOUTH -> WEST
+                EAST -> SOUTH
+                WEST -> NORTH
+            }
+        }
     }
 
-    fun addTwoNumbers(a: Int, b: Int): Int {
-        return a + b
+    class Grid {
+        private var direction = Direction.NORTH
+        private var location = Pair(0, 0)
+        private var visited = mutableListOf(Pair(0, 0))
+        fun move(command: String){
+            this.direction = direction.turn(command[0])
+            val distance = command.substring(1).toInt()
+            location = move(distance)
+        }
+
+        private fun move(distance: Int): Pair<Int, Int> {
+            return when (this.direction) {
+                Direction.NORTH -> moveNorth(distance)
+                Direction.SOUTH -> moveSouth(distance)
+                Direction.EAST -> moveEast(distance)
+                Direction.WEST -> moveWest(distance)
+            }
+        }
+
+        private fun moveNorth(distance: Int): Pair<Int, Int> {
+            val startY = location.second
+            val endY = location.second + distance
+
+            for (i in startY+1..endY) {
+                visited.add(Pair(location.first, i))
+            }
+            return Pair(location.first, endY)
+        }
+
+        private fun moveSouth(distance: Int): Pair<Int, Int> {
+            val startY = location.second
+            val endY = location.second - distance
+
+            for (i in startY-1 downTo endY) {
+                visited.add(Pair(location.first, i))
+            }
+            return Pair(location.first, endY)
+        }
+
+        private fun moveEast(distance: Int): Pair<Int, Int> {
+            val startX = location.first
+            val endX = location.first + distance
+
+            for (i in startX+1..endX) {
+                visited.add(Pair(i, location.second))
+            }
+            return Pair(endX, location.second)
+        }
+
+        private fun moveWest(distance: Int): Pair<Int, Int> {
+            val startX = location.first
+            val endX = location.first - distance
+
+            for (i in startX-1 downTo endX) {
+                visited.add(Pair(i, location.second))
+            }
+            return Pair(endX, location.second)
+        }
+
+        fun getBlocksAway(): Int {
+            return getBlocksAway(location)
+        }
+        private fun getBlocksAway(pos: Pair<Int,Int>): Int {
+            return abs(pos.first) + abs(pos.second)
+        }
+
+        fun getFirstVisitedTwice(): Int {
+            val moreThanOnce = visited.groupingBy { it }.eachCount().filter { it.value > 1 }
+            return getBlocksAway(moreThanOnce.keys.first())
+        }
     }
 }
--- src/test/kotlin/Day1Test.kt
+++ src/test/kotlin/Day1Test.kt
@@ -4,15 +4,60 @@ import org.junit.jupiter.api.Assertions.*
 
 class Day1Test {
 
+//    Following R2, L3 leaves you 2 blocks East and 3 blocks North, or 5 blocks away.
+//    R2, R2, R2 leaves you 2 blocks due South of your starting position, which is 2 blocks away.
+//    R5, L5, R5, R3 leaves you 12 blocks away.
+
     @Test
-    fun day1() {
-        val data = Util().readData("fred.txt")
-        assertEquals(2,data.size)
+    fun firstSample() {
+        val grid = Day1.Grid()
+        grid.move("R2")
+        grid.move("L3")
+        assertEquals(5,grid.getBlocksAway())
     }
 
     @Test
-    fun addTwoNumbers() {
-        val day1 = Day1()
-        assertEquals(3, day1.addTwoNumbers(1, 2))
+    fun secondSample() {
+        val grid = Day1.Grid()
+        grid.move("R2")
+        grid.move("R2")
+        grid.move("R2")
+        assertEquals(2,grid.getBlocksAway())
+    }
+
+    @Test
+    fun thirdSample() {
+        val grid = Day1.Grid()
+
+        grid.move("R5")
+        grid.move("L5")
+        grid.move("R5")
+        grid.move("R3")
+
+        assertEquals(12,grid.getBlocksAway())
+    }
+
+    @Test
+    fun part1() {
+        val commands = Util().readData("day1.txt")[0].split(", ")
+        val grid = Day1.Grid()
+        commands.forEach { grid.move(it) }
+        assertEquals(287, grid.getBlocksAway())
+    }
+
+    @Test
+    fun part2Sample(){
+        val commands = "R8, R4, R4, R8".split(", ")
+        val grid = Day1.Grid()
+        commands.forEach { grid.move(it) }
+        assertEquals(4, grid.getFirstVisitedTwice())
+    }
+
+    @Test
+    fun part2() {
+        val commands = Util().readData("day1.txt")[0].split(", ")
+        val grid = Day1.Grid()
+        commands.forEach { grid.move(it) }
+        assertEquals(133, grid.getFirstVisitedTwice())
     }
 }
