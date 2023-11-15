e6659d0f9a5594f13828442ea365f567a57c006f
martwebs <mwebster1973@gmail.com>
2023-11-15 16:39:13 +0000

    Initial Commit

---
+++ src/main/kotlin/Day1.kt
@@ -0,0 +1,9 @@
+class Day1 {
+    fun day1(): Int {
+        return 10
+    }
+
+    fun addTwoNumbers(a: Int, b: Int): Int {
+        return a + b
+    }
+}
---
+++ src/test/kotlin/Day1Test.kt
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class Day1Test {
+
+    @Test
+    fun day1() {
+        val data = Util().readData("fred.txt")
+        assertEquals(2,data.size)
+    }
+
+    @Test
+    fun addTwoNumbers() {
+        val day1 = Day1()
+        assertEquals(3, day1.addTwoNumbers(1, 2))
+    }
+}
